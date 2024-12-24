778ab4ac3eb93c1868c6c3f73d405c5df21472a9
apirak_kam <apirak_kam@rtaf.mi.th>
2024-12-24 09:27:08 +0700

    update_uno

--- testcmd.py
+++ testcmd.py
@@ -7,7 +7,7 @@ def send_command(command,direction,step):
       command: The command to send (e.g., "go", "left", "stop").
   """
   # url = f"http://192.168.55.92/{command}?{int(time.time()*1000)}"  # Using time.time() for timestamp
-  url = f"http://192.168.137.35/{command}?direction={direction}&step={step}"  # Using time.time() for timestamp
+  url = f"http://192.168.137.127/{command}?direction={direction}&step={step}"  # Using time.time() for timestamp
   response = requests.get(url)
 
   # Handle response (optional)
@@ -18,6 +18,7 @@ def send_command(command,direction,step):
   else:
     print(f"Error sending command: {response.status_code}")
 
+send_command("step_command", "forward", "1000")
 send_command("step_command", "stop", "1000")
 # Example usage
 #   send_command("step_command", "forward", "1000")
